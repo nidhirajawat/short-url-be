30079c8ba23319fd0221b97fc588e63fcd57405b
Nikita Rajawat <nikita@gmail.com>
2024-11-22 21:16:44 +0530

    node js server start

---
+++ app.js
@@ -0,0 +1,18 @@
+// require('dotenv').config({ path: './.env.dev' }); // Ensure this points to your .env file
+
+const express = require('express');
+const app = express();
+
+const hostname = process.env.HOSTNAME ?? "localhost";
+const port = process.env.PORT ?? "5000";
+
+//GET
+app.get('/', (req, res) => {
+    res.send('Heloooo worldd');
+})
+
+//server start process
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
